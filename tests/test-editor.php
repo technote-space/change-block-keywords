bf434bd1c4f9ea0e85282b4aba6bd0ed66762a55
Technote <technote.space@gmail.com>
2019-07-01 17:37:54 +0900

    update test

--- tests/test-editor.php
+++ tests/test-editor.php
@@ -17,7 +17,7 @@ use PHPUnit\Framework\TestCase;
 class EditorTest extends WP_UnitTestCase {
 
 	/**
-	 * @var WP_Framework|Phake_IMock
+	 * @var WP_Framework
 	 */
 	protected static $app;
 
@@ -32,16 +32,24 @@ class EditorTest extends WP_UnitTestCase {
 	public static function setUpBeforeClass() {
 		static::$app    = WP_Framework::get_instance( CHANGE_BLOCK_KEYWORDS );
 		static::$editor = Editor::get_instance( static::$app );
+		static::reset();
+	}
+
+	public static function tearDownAfterClass() {
+		static::reset();
+		static::$app->file->delete( static::$app->define->plugin_assets_dir . DS . 'js' . DS . 'index.min.js' );
+	}
+
+	private static function reset() {
+		wp_dequeue_script( 'change-block-keywords' );
+		static::$app->file->put_contents( static::$app->define->plugin_assets_dir . DS . 'js' . DS . 'index.min.js', '' );
 	}
 
 	public function test_enqueue_block_editor_assets() {
-		do_action( 'enqueue_block_editor_assets' );
-		$wp_scripts = wp_scripts();
+		static::reset();
 
-		$this->assertArrayHasKey( 'change-block-keywords', $wp_scripts->registered );
-		$this->assertStringEndsWith( '/assets/js/index.min.js', $wp_scripts->registered['change-block-keywords']->src );
-		$this->assertContains( 'change-block-keywords', $wp_scripts->registered['change-block-keywords']->src );
-		$this->assertArrayHasKey( 'data', $wp_scripts->registered['change-block-keywords']->extra );
-		$this->assertContains( ' cbkParams ', $wp_scripts->registered['change-block-keywords']->extra['data'] );
+		$this->assertFalse( wp_script_is( 'change-block-keywords' ) );
+		do_action( 'enqueue_block_editor_assets' );
+		$this->assertTrue( wp_script_is( 'change-block-keywords' ) );
 	}
 }
