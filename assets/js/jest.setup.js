9742239e501cfb7f092f8d91d391de215cc93ef4
Technote <technote.space@gmail.com>
2019-09-09 17:45:41 +0900

    feat: update tests

--- assets/js/jest.setup.js
+++ assets/js/jest.setup.js
@@ -14,48 +14,12 @@ global.window.matchMedia = () => ( {
 	matches: true, addListener: () => {
 	},
 } );
-global.wpMock = {
-	blockEditor: {
-		getColorObjectByColorValue: () => false,
-	},
-	element: {
-		useRef: () => ( {
-			current: {
-				contains: () => false,
-				focus: () => 0,
-				getBoundingClientRect: () => ( { width: 0, height: 0 } ),
-				parentNode: {
-					getBoundingClientRect: () => ( { width: 0, height: 0, left: 0, right: 0, top: 0, bottom: 0 } ),
-				},
-				querySelectorAll: () => ( [] ),
-			},
-		} ),
-	},
-};
-global.window.lodash.debounce = fn => {
-	function debounced() {
-		return fn();
-	}
-
-	debounced.cancel = jest.fn();
-	debounced.flush = jest.fn();
-	return debounced;
-};
 global.cbkParams = {
 	translate: {},
 };
 
-jest.mock( '@wordpress/block-editor', () => ( {
-	...jest.requireActual( '@wordpress/block-editor' ),
-	getColorObjectByColorValue: ( colors, value ) => global.wpMock.blockEditor.getColorObjectByColorValue( colors, value ),
-} ) );
-jest.mock( '@wordpress/element', () => ( {
-	...jest.requireActual( '@wordpress/element' ),
-	useRef: ( colors, value ) => global.wpMock.element.useRef( colors, value ),
-} ) );
-
-const blockLibrary = require( '@wordpress/block-library' );
 const blockEditor = require( '@wordpress/block-editor' );
+const blockLibrary = require( '@wordpress/block-library' );
 const blocks = require( '@wordpress/blocks' );
 const components = require( '@wordpress/components' );
 const compose = require( '@wordpress/compose' );
@@ -64,6 +28,7 @@ const data = require( '@wordpress/data' );
 const dom = require( '@wordpress/dom' );
 const editor = require( '@wordpress/editor' );
 const element = require( '@wordpress/element' );
+const formatLibrary = require( '@wordpress/format-library' );
 const hooks = require( '@wordpress/hooks' );
 const i18n = require( '@wordpress/i18n' );
 const isShallowEqual = require( '@wordpress/is-shallow-equal' );
@@ -72,8 +37,8 @@ const richText = require( '@wordpress/rich-text' );
 const url = require( '@wordpress/url' );
 
 global.wp = {
-	blockLibrary,
 	blockEditor,
+	blockLibrary,
 	blocks,
 	components,
 	compose,
@@ -82,6 +47,7 @@ global.wp = {
 	dom,
 	editor,
 	element,
+	formatLibrary,
 	hooks,
 	i18n,
 	isShallowEqual,
