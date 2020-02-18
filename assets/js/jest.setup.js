2fe969358ca981561ba6935bda57a21d4cd0cdfd
Technote <technote.space@gmail.com>
2020-02-18 18:42:00 +0900

    feat: use library helper

--- assets/js/jest.setup.js
+++ assets/js/jest.setup.js
@@ -1,61 +1,11 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { setupGlobal } from '@technote-space/gutenberg-test-helper';
 
-Enzyme.configure( {
-	adapter: new Adapter(),
-	snapshotSerializers: [ 'enzyme-to-json/serializer' ],
-} );
-
-const Mousetrap = require( 'mousetrap' );
-const lodash = require( 'lodash' );
-global.Mousetrap = Mousetrap;
-global.window.lodash = lodash;
-global.window.matchMedia = () => ( {
-	matches: true, addListener: () => {
+setupGlobal( {
+	setUseRefMock: false,
+	mockLodashDebounce: false,
+	globalParams: {
+		cbkParams: {
+			translate: {},
+		},
 	},
 } );
-global.cbkParams = {
-	translate: {},
-};
-
-const blockEditor = require( '@wordpress/block-editor' );
-const blockLibrary = require( '@wordpress/block-library' );
-const blocks = require( '@wordpress/blocks' );
-const components = require( '@wordpress/components' );
-const compose = require( '@wordpress/compose' );
-const coreData = require( '@wordpress/core-data' );
-const data = require( '@wordpress/data' );
-const dom = require( '@wordpress/dom' );
-const editor = require( '@wordpress/editor' );
-const element = require( '@wordpress/element' );
-const formatLibrary = require( '@wordpress/format-library' );
-const hooks = require( '@wordpress/hooks' );
-const i18n = require( '@wordpress/i18n' );
-const isShallowEqual = require( '@wordpress/is-shallow-equal' );
-const keycodes = require( '@wordpress/keycodes' );
-const richText = require( '@wordpress/rich-text' );
-const url = require( '@wordpress/url' );
-
-global.wp = {
-	blockEditor,
-	blockLibrary,
-	blocks,
-	components,
-	compose,
-	coreData,
-	data,
-	dom,
-	editor,
-	element,
-	formatLibrary,
-	hooks,
-	i18n,
-	isShallowEqual,
-	keycodes,
-	richText,
-	url,
-};
-
-blockLibrary.registerCoreBlocks();
-
-hooks.removeAllFilters( 'editor.BlockEdit' );
