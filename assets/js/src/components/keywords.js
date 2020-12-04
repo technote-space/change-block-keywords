c1cca3cc6d4d197b89880018452872b58cc59549
Technote <technote.space@gmail.com>
2020-12-05 00:09:25 +0900

    perf: use react hooks to cache

--- assets/js/src/components/keywords.js
+++ assets/js/src/components/keywords.js
@@ -2,33 +2,31 @@ import React from 'react';
 import { Helpers } from '@technote-space/gutenberg-utils';
 import { InspectorAdvancedControls } from '@wordpress/block-editor';
 import { FormTokenField } from '@wordpress/components';
-import { compose } from '@wordpress/compose';
-import { withSelect, withDispatch } from '@wordpress/data';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { useMemo, useCallback } from '@wordpress/element';
 import { STORE_NAME } from '../store/constant';
 
 const { getTranslator } = Helpers;
+/** @var {object} cbkParams */
 const translate         = getTranslator(cbkParams);
 
 /**
  * @param {object} props props
- * @param {function} keywords keywords
- * @param {function} setKeywords set keywords
  * @returns {Component} input keywords form
  * @constructor
  */
-const Keywords = ({ props, keywords, setKeywords }) => <InspectorAdvancedControls>
-  <FormTokenField
-    value={keywords(props)}
-    onChange={tokens => setKeywords(props, tokens)}
-    label={translate('Set Search Keywords')}
-  />
-</InspectorAdvancedControls>;
+const Keywords = ({ props: { name } }) => {
+  const keywords        = useSelect(select => select(STORE_NAME).getKeywords(name), [name]);
+  const { setKeywords } = useDispatch(STORE_NAME);
+  const onChange        = useCallback(keywords => setKeywords(name, keywords), [name]);
+  const label           = useMemo(() => translate('Set Search Keywords'), []);
+  return useMemo(() => <InspectorAdvancedControls>
+    <FormTokenField
+      value={keywords}
+      onChange={onChange}
+      label={label}
+    />
+  </InspectorAdvancedControls>, [name, keywords]);
+};
 
-export default compose(
-  withSelect(select => ({
-    keywords: props => select(STORE_NAME).getKeywords(props.name),
-  })),
-  withDispatch(dispatch => ({
-    setKeywords: (props, keywords) => dispatch(STORE_NAME).setKeywords(props.name, keywords),
-  })),
-)(Keywords);
+export default Keywords;
