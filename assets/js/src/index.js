ba16d270853a53c6a576240f72b596010c2160cd
Technote <technote.space@gmail.com>
2020-12-05 03:21:04 +0900

    chore: tweaks

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -1,16 +1,11 @@
 import { addFilter } from '@wordpress/hooks';
-import { Helpers } from '@technote-space/gutenberg-utils';
 import './store';
 import { getNamespace, getKeywordsFormComponent, getSetupKeywordsFunc } from './utils';
-
-const { getTranslator } = Helpers;
-/** @var {object} cbkParams */
-const translate         = getTranslator(cbkParams);
 
 addFilter(
   'editor.BlockEdit',
   getNamespace('add-keywords-form'),
-  getKeywordsFormComponent(translate('Set Search Keywords')),
+  getKeywordsFormComponent(),
 );
 
 addFilter(
